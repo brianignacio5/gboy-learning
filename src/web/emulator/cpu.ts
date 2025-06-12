2ee48a3099e3507c4a64b6214ad5f7e212df1b36
Brian Ignacio <brianignacio5@gmail.com>
2025-06-12 21:58:16 +0800

    instructions 20 to 42

--- src/web/emulator/cpu.ts
+++ src/web/emulator/cpu.ts
@@ -143,6 +143,42 @@ export class CPU {
     instructions[0x1e] = this.ldE_d8;
     instructions[0x1f] = this.rrA;
 
+    instructions[0x20] = this.jrNZ_r8;
+    instructions[0x21] = this.ldHL_d16;
+    instructions[0x22] = this.ldHL_A;
+    instructions[0x23] = this.incHL;
+    instructions[0x24] = this.incH;
+    instructions[0x25] = this.decH;
+    instructions[0x26] = this.ldH_d8;
+    instructions[0x27] = this.daa;
+    instructions[0x28] = this.jrZ_r8;
+    instructions[0x29] = this.addHLHL;
+    instructions[0x2a] = this.ldA_HL;
+    instructions[0x2b] = this.decHL;
+    instructions[0x2c] = this.incL;
+    instructions[0x2d] = this.decL;
+    instructions[0x2e] = this.ldL_d8;
+    instructions[0x2f] = this.cpl;
+    instructions[0x30] = this.jrNC_r8;
+    instructions[0x31] = this.ldSP_d16;
+    instructions[0x32] = this.ldHL_A;
+    instructions[0x33] = this.incSP;
+    instructions[0x34] = this.incHL;
+    instructions[0x35] = this.decHL;
+    instructions[0x36] = this.ldHL_d8;
+    instructions[0x37] = this.scf;
+    instructions[0x38] = this.jrC_r8;
+    instructions[0x39] = this.addHLSP;
+    instructions[0x3a] = this.ldA_HL;
+    instructions[0x3b] = this.decSP;
+    instructions[0x3c] = this.incA;
+    instructions[0x3d] = this.decA;
+    instructions[0x3e] = this.ldA_d8;
+    instructions[0x3f] = this.ccf;
+    instructions[0x40] = this.ldB_B;
+    instructions[0x41] = this.ldB_C;
+    instructions[0x42] = this.ldB_D;
+
     return instructions;
   }
 
@@ -200,6 +236,20 @@ export class CPU {
     this.clocks.t = 12;
   }
 
+  private ldSP_d16() {
+    const value = this.fetchWord();
+    this.registers.sp = value;
+    this.clocks.m = 3;
+    this.clocks.t = 12;
+  }
+
+  private ldHL_d16() {
+    const value = this.fetchWord();
+    this.registers.hl = value;
+    this.clocks.m = 3;
+    this.clocks.t = 12;
+  }
+
   private ldB_d8() {
     const value = this.fetch();
     this.registers.b = value;
@@ -228,6 +278,27 @@ export class CPU {
     this.clocks.t = 8;
   }
 
+  private ldH_d8() {
+    const value = this.fetch();
+    this.registers.h = value;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  private ldL_d8() {
+    const value = this.fetch();
+    this.registers.l = value;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  private ldHL_d8() {
+    const value = this.fetch();
+    this.memory.writeByte(this.registers.hl, value);
+    this.clocks.m = 3;
+    this.clocks.t = 12;
+  }
+
   private ld_a16_SP() {
     const address = this.fetchWord();
     this.memory.writeByte(address, this.registers.sp & 0xff); // Lower Byte
@@ -251,6 +322,13 @@ export class CPU {
     this.clocks.t = 8;
   }
 
+  private ldHL_A() {
+    this.memory.writeByte(this.registers.hl, this.registers.a);
+    this.registers.hl = (this.registers.hl + 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
   private ldA_BC() {
     this.registers.a = this.memory.readByte(this.registers.bc);
     this.clocks.m = 2;
@@ -261,6 +339,42 @@ export class CPU {
     this.registers.a = this.memory.readByte(this.registers.de);
     this.clocks.m = 2;
     this.clocks.t = 8;
+  }
+
+  private ldA_HL() {
+    this.registers.a = this.memory.readByte(this.registers.hl);
+    this.registers.hl = (this.registers.hl + 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  /**
+   * Load B into B (LD B, B).
+   * Copies the value of the B register into itself.
+   * This is effectively a no-operation (NOP) for the B register.
+   */
+  private ldB_B() {
+    this.registers.b = this.registers.b;
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  /**
+   * Load the value of register C into register B (LD B, C).
+   */
+  private ldB_C() {
+    this.registers.b = this.registers.c;
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  /**
+   * Load the value of register D into register B (LD B, D).
+   */
+  private ldB_D() {
+    this.registers.b = this.registers.d;
+    this.clocks.m = 1;
+    this.clocks.t = 4;
   }
 
   private incBC() {
@@ -275,8 +389,68 @@ export class CPU {
     this.clocks.t = 8;
   }
 
+  private incHL() {
+    this.registers.hl = (this.registers.hl + 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  private incSP() {
+    this.registers.sp = (this.registers.sp + 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  private decSP() {
+    this.registers.sp = (this.registers.sp - 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  /**
+   * Increment A (INC A).
+   * Increments the value of the A register by 1.
+   *
+   * Flags affected:
+   * - Z (Zero): Set if the result is 0.
+   * - N (Subtract): Cleared to 0.
+   * - H (Half Carry): Set if there is a carry from bit 3 to bit 4.
+   * - C (Carry): Unaffected.
+   *
+   * Clock cycles: 4
+   */
+  private incA() {
+    this.registers.a = (this.registers.a + 1) & 0xff;
+    this.setFlag("Z", this.registers.a === 0);
+    this.setFlag("N", false);
+    this.setFlag("H", (this.registers.a & 0x0f) === 0);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  /**
+   * Decrement A (DEC A).
+   * Decrements the value of the A register by 1.
+   *
+   * Flags affected:
+   * - Z (Zero): Set if the result is 0.
+   * - N (Subtract): Set to 1.
+   * - H (Half Carry): Set if there is a borrow from bit 4 to bit 3.
+   * - C (Carry): Unaffected.
+   *
+   * Clock cycles: 4
+   */
+  private decA() {
+    this.registers.a = (this.registers.a - 1) & 0xff;
+    this.setFlag("Z", this.registers.a === 0);
+    this.setFlag("N", true);
+    this.setFlag("H", (this.registers.a & 0x0f) === 0x0f);
+    this.clocks.m = 1;
+    this.clocks.t = 4; 
+  }
+
   private incB() {
-    this.registers.bc = (this.registers.b + 1) & 0xffff;
+    this.registers.b = (this.registers.b + 1) & 0xffff;
     this.setFlag("Z", this.registers.b === 0);
     this.setFlag("N", false);
     this.setFlag("H", (this.registers.b & 0x0f) === 0);
@@ -285,7 +459,7 @@ export class CPU {
   }
 
   private decB() {
-    this.registers.bc = (this.registers.b - 1) & 0xffff;
+    this.registers.b = (this.registers.b - 1) & 0xffff;
     this.setFlag("Z", this.registers.b === 0);
     this.setFlag("N", true);
     this.setFlag("H", (this.registers.b & 0x0f) === 0x0f);
@@ -347,6 +521,42 @@ export class CPU {
     this.clocks.t = 4;
   }
 
+  private incH() {
+    this.registers.h = (this.registers.h + 1) & 0xff;
+    this.setFlag("Z", this.registers.h === 0);
+    this.setFlag("N", false);
+    this.setFlag("H", (this.registers.h & 0x0f) === 0);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  private decH() {
+    this.registers.h = (this.registers.h - 1) & 0xff;
+    this.setFlag("Z", this.registers.h === 0);
+    this.setFlag("N", true);
+    this.setFlag("H", (this.registers.h & 0x0f) === 0x0f);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  private incL() {
+    this.registers.l = (this.registers.l + 1) & 0xff;
+    this.setFlag("Z", this.registers.l === 0);
+    this.setFlag("N", false);
+    this.setFlag("H", (this.registers.l & 0x0f) === 0);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  private decL() {
+    this.registers.l = (this.registers.l - 1) & 0xff;
+    this.setFlag("Z", this.registers.l === 0);
+    this.setFlag("N", true);
+    this.setFlag("H", (this.registers.l & 0x0f) === 0x0f);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
   private decBC() {
     this.registers.bc = (this.registers.bc - 1) & 0xffff;
     this.clocks.m = 2;
@@ -355,6 +565,12 @@ export class CPU {
 
   private decDE() {
     this.registers.de = (this.registers.de - 1) & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  private decHL() {
+    this.registers.hl = (this.registers.hl - 1) & 0xffff;
     this.clocks.m = 2;
     this.clocks.t = 8;
   }
@@ -459,12 +675,219 @@ export class CPU {
     this.clocks.t = 8;
   }
 
+  /**
+   * Add HL to HL
+   * H: Set if there is carry in bit 11
+   * C: Set if there is a carry in bit 15
+   * N: reset
+   */
+  private addHLHL() {
+    const result = this.registers.hl + this.registers.hl;
+    this.setFlag("C", result > 0xffff);
+    this.setFlag(
+      "H",
+      (this.registers.hl & 0xfff) + (this.registers.hl & 0xfff) > 0xfff
+    );
+    this.setFlag("N", false);
+    this.registers.hl = result & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
+  /**
+   * Add SP to HL (ADD HL, SP).
+   * Adds the value of the SP register to the HL register pair.
+   *
+   * Flags affected:
+   * - Z (Zero): Unaffected.
+   * - N (Subtract): Cleared to 0.
+   * - H (Half Carry): Set if there is a carry from bit 11.
+   * - C (Carry): Set if there is a carry from bit 15.
+   *
+   * Clock cycles: 8
+   */
+  private addHLSP() {
+    const result = this.registers.hl + this.registers.sp;
+    this.setFlag("C", result > 0xffff);
+    this.setFlag(
+      "H",
+      (this.registers.hl & 0xfff) + (this.registers.sp & 0xfff) > 0xfff
+    );
+    this.setFlag("N", false);
+    this.registers.hl = result & 0xffff;
+    this.clocks.m = 2;
+    this.clocks.t = 8;
+  }
+
   private jr_r8() {
     const byte = this.memory.readByte(this.registers.pc + 1) & 0xffff;
     const offset = this.getSignedOffsetByte(byte);
     this.registers.pc = (this.registers.pc + offset) & 0xffff;
     this.clocks.m = 3;
     this.clocks.t = 12;
+  }
+
+  /**
+   * Jump relative to the current program counter (PC) by adding
+   * an 8-bit signed value if the Zero (Z) flag is not set.
+   */
+  private jrNZ_r8() {
+    if ((this.registers.f & 0x80) === 0) {
+      const offset = this.getSignedOffsetByte(this.fetch());
+      this.registers.pc = (this.registers.pc + offset) & 0xffff;
+      this.clocks.m = 3;
+      this.clocks.t = 12;
+    } else {
+      this.registers.pc = (this.registers.pc + 1) & 0xffff;
+      this.clocks.m = 2;
+      this.clocks.t = 8;
+    }
+  }
+
+  /**
+   * Jump relative to the current program counter (PC) by adding
+   * an 8-bit signed value if the Zero (Z) flag is set.
+   */
+  private jrZ_r8() {
+    if ((this.registers.f & 0x80) !== 0) {
+      // Check if Zero flag (Z) is set
+      const offset = this.getSignedOffsetByte(this.fetch());
+      this.registers.pc = (this.registers.pc + offset) & 0xffff;
+      this.clocks.m = 3;
+      this.clocks.t = 12;
+    } else {
+      this.registers.pc = (this.registers.pc + 1) & 0xffff;
+      this.clocks.m = 2;
+      this.clocks.t = 8;
+    }
+  }
+
+  /**
+   * Jump relative to the current program counter (PC) by adding
+   * an 8-bit signed value if the Carry (C) flag is not set.
+   */
+  private jrNC_r8() {
+    if ((this.registers.f & 0x10) === 0) {
+      const offset = this.getSignedOffsetByte(this.fetch());
+      this.registers.pc = (this.registers.pc + offset) & 0xffff;
+      this.clocks.m = 3;
+      this.clocks.t = 12;
+    } else {
+      this.registers.pc = (this.registers.pc + 1) & 0xffff;
+      this.clocks.m = 2;
+      this.clocks.t = 8;
+    }
+  }
+
+  /**
+   * Jump relative to the current program counter (PC) by adding
+   * an 8-bit signed value if the Carry (C) flag is set.
+   *
+   * Flags affected:
+   * - Z (Zero): Unaffected.
+   * - N (Subtract): Unaffected.
+   * - H (Half Carry): Unaffected.
+   * - C (Carry): Checked to determine if the jump is taken.
+   *
+   * Clock cycles:
+   * - 12 cycles if the jump is taken.
+   * - 8 cycles if the jump is not taken.
+   */
+  private jrC_r8() {
+    if ((this.registers.f & 0x10) !== 0) {
+      const offset = this.getSignedOffsetByte(this.fetch());
+      this.registers.pc = (this.registers.pc + offset) & 0xffff;
+      this.clocks.m = 3;
+      this.clocks.t = 12;
+    } else {
+      this.registers.pc = (this.registers.pc + 1) & 0xffff;
+      this.clocks.m = 2;
+      this.clocks.t = 8;
+    }
+  }
+
+  /**
+   * Decimal Adjust Accumulator (DAA).
+   * Adjusts the value in the A register to ensure it is a valid Binary-Coded Decimal (BCD) value.
+   * This is typically used after addition or subtraction operations.
+   *
+   * Flags affected:
+   * - Z (Zero): Set if the result in A is 0.
+   * - N (Subtract): Unchanged.
+   * - H (Half Carry): Cleared.
+   * - C (Carry): Set or cleared based on the adjustment.
+   *
+   * Clock cycles: 4
+   */
+  private daa() {
+    let correction = 0;
+    const isNegative = (this.registers.f & 0x40) !== 0;
+
+    if (
+      (this.registers.f & 0x20) !== 0 ||
+      (!isNegative && (this.registers.a & 0x0f) > 9)
+    ) {
+      correction |= 0x06;
+    }
+
+    if (
+      (this.registers.f & 0x10) !== 0 ||
+      (!isNegative && this.registers.a > 0x99)
+    ) {
+      correction |= 0x60;
+      this.setFlag("C", true);
+    } else {
+      this.setFlag("C", false);
+    }
+
+    this.registers.a = isNegative
+      ? (this.registers.a - correction) & 0xff
+      : (this.registers.a + correction) & 0xff;
+
+    this.setFlag("Z", this.registers.a === 0);
+    this.setFlag("H", false);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  /**
+   * Complement (invert) all bits in the A register.
+   * This operation flips each bit in the A register (1 becomes 0, and 0 becomes 1).
+   *
+   * Flags affected:
+   * - Z (Zero): Unaffected.
+   * - N (Subtract): Set to 1.
+   * - H (Half Carry): Set to 1.
+   * - C (Carry): Unaffected.
+   *
+   * Clock cycles: 4
+   */
+  private cpl() {
+    this.registers.a = this.registers.a ^ 0xff;
+    this.setFlag("N", true);
+    this.setFlag("H", true);
+    this.clocks.m = 1;
+    this.clocks.t = 4;
+  }
+
+  /**
+   * Set Carry Flag (SCF).
+   * Sets the Carry (C) flag to 1 and clears the Subtract (N) and Half Carry (H) flags.
+   *
+   * Flags affected:
+   * - Z (Zero): Unaffected.
+   * - N (Subtract): Cleared to 0.
+   * - H (Half Carry): Cleared to 0.
+   * - C (Carry): Set to 1.
+   *
+   * Clock cycles: 4
+   */
+  private scf() {
+    this.setFlag("C", true); // Set Carry flag
+    this.setFlag("N", false); // Clear Subtract flag
+    this.setFlag("H", false); // Clear Half Carry flag
+    this.clocks.m = 1; // 4 clock cycles
+    this.clocks.t = 4;
   }
 
   private stop() {
